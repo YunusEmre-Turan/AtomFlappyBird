cd428421733d513e6f9706e06c3702d9f247964c
ynsmr.trn256@hotmail.com <ynsmr.trn256@hotmail.com>
2022-03-15 17:52:58 +0300

    Proje dosyası ekle.

---
+++ Assets/sCRİPTS/Birdy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdy : MonoBehaviour
+{
+    public bool isDead;
+
+    public Rigidbody2D rb2D;
+    public float velocity = 1f;
+    public GameManager managerGame;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //zıplatma
+            rb2D.velocity = Vector2.up * velocity;
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+     
+       
+        {
+            if(collision.gameObject.name== "ScoreArea")
+             {
+             managerGame.UpdateScore();
+             }
+        }
+}
---
+++ Assets/sCRİPTS/Spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Birdy BirdScript;
+    public GameObject Borular;
+    public float height;
+    public float time;                                          // burdaki
+
+    private void Start()
+    {
+        StartCoroutine(SpawnObject(time));                      // ve burdaki birbiriyle aynı 
+    }
+
+    public IEnumerator SpawnObject(float time)                                          //bu (1)
+    {
+        while (!BirdScript.isDead)
+        {
+            Instantiate(Borular, new Vector3(3, Random.Range(-height, height), 0), Quaternion.identity);
+
+            yield return new WaitForSeconds(time);                                      // ve bu da aynı(1)
+           
+        }
+    }
+}
